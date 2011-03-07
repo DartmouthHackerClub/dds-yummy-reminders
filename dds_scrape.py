a87cdc4379486c90c2d3e2d130ac2cf1abb881ea
Ian <ianw@dartmouth.edu>
2011-03-07 14:48:30 -0500

    added date to cron script, better string matching

--- dds_scrape.py
+++ dds_scrape.py
@@ -1,22 +1,29 @@
 import urllib2, urllib, webbrowser
 from BeautifulSoup import BeautifulSoup
 import re
+import difflib
 
 def removeTags(thing):
     for tag in thing.findAll(True): tag.replaceWith(tag.renderContents())
 
-def entities(string):
-    return re.sub("&nbsp;", " ", re.sub("&amp;", "&", string))
+def unescape(s):
+  s = s.replace("&lt;", "<")
+  s = s.replace("&gt;", ">")
+  # this has to be last:
+  s = s.replace("&amp;", "&")
+  s = s.replace("&nbsp;", " ")
+  return s
     
 def removeHTML(string):
     return re.sub("<(.|\n)*?>", "", string)
     
 def cleanString(string):
-    return entities(removeHTML(string)).strip()
+    return removeHTML(string).strip()
 
 def noms():
-        print 'Loading page'
+        print 'Downloading page...'
         page = urllib2.urlopen("http://www.dartmouth.edu/dining/")
+        print 'Page downloaded.'
         soup = BeautifulSoup(page)
         sidebar = soup.find("div", {"class":"a1"})
         noms = []
@@ -29,16 +36,23 @@ def noms():
                 parts = thing.renderContents().split(":")
                 if (len(parts) > 1):
                     category = parts[0]
-                    for foodItem in parts[1].replace(";",",").split(","):
+                    for foodItem in unescape(parts[1]).replace(";",",").split(","):
                         noms.append((cleanString(foodItem), cleanString(category), currentPlace))
+        print 'Page parsed.'
         return noms
 
 def stringInString(needle, haystack):
     return haystack.lower().find(needle.lower()) > -1
 
+def close_match(stringA, stringB):
+	return len(difflib.get_close_matches(stringA, [stringB])) > 0
+
+def removePunctuation(text):
+	return re.sub("\s+", "", re.sub("[^A-z0-9]","",text))
+
 noms = noms()
         
 def isThere(nomName):
     global noms
-    def isRightNom(x): return stringInString(nomName,x[0])
+    def isRightNom(x): return stringInString(removePunctuation(nomName),removePunctuation(x[0]))
     return filter(isRightNom, noms)
--- mailer.sh
+++ mailer.sh
@@ -1,3 +1,4 @@
 export DJANGO_SETTINGS_MODULE="scrape.settings"
 export PYTHONPATH="/var/www"
+date
 python /var/www/scrape/mailer.py
